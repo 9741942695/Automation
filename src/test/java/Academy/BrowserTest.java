5fdcc9310dfcf748a7174e98fd93628b76c25e69
akidang <mail2akashdang@gmail.com>
2026-09-02 18:37:33 +0530

    third

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -12,7 +12,7 @@ public class BrowserTest {
 	@Test
 	public void getData()
 	{
-		System.out.println("Hellos Guys");
+		System.out.println("Hello Guys");
 		WebDriver driver =new ChromeDriver();
 		driver.get("https://www.google.com");
 		driver.close();
