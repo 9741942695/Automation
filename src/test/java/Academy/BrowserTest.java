6ba2bd06aa7b1c3a81e7e746b635755da135ff47
akidang <mail2akashdang@gmail.com>
2026-09-03 12:16:36 +0530

    new

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -3,21 +3,26 @@ package Academy;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 
 
 public class BrowserTest {
 
-	
+	WebDriver driver;
 	@Test
 	public void getData()
 	{
 		System.out.println("Hello Guy");
-		WebDriver driver =new ChromeDriver();
-		driver.get("https://www.google.com");
+		String browser = System.getProperty("browser", "safari");
+		if(browser.equalsIgnoreCase("chrome")) {
+		driver=new ChromeDriver();
+		}
+		if(browser.equalsIgnoreCase("safari")) {
+			driver=new SafariDriver();
+		}
+			driver.get("https://www.google.com");
 		driver.close();
 	
-		
-		
 	}
 }
